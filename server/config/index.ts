699ab73b5bd44ee090b0fca51068eee8d03b207b
Nickds54 <nicksalter98@gmail.com>
2020-08-14 00:54:02 -0500

    Git ignore is now working

--- server/config/index.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    mysql: { 
-      host: 'salterdb.c9xvxoh8uzch.us-east-2.rds.amazonaws.com',
-      user: 'Nickds54',
-      password: 'Admin***',
-      port: 3306,
-      database: "salterdb"
-    }
-  }
